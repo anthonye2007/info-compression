794dd0d84750f3c629707b73fc3bafb736498cd9
anthony_elliott <ellioaac@uni.edu>
2014-03-06 15:39:15 -0600

    Print length of word as a single byte

--- compression.rb
+++ compression.rb
@@ -3,7 +3,23 @@
 # Information Storage and Retrieval
 # 3/6/2014
 
+# size, base word * remainder of first word * remainder of second word
+
 input = File.read(ARGV.first)
 words = input.split("\n")
 puts words.length.to_s
-puts words.first
+
+=begin
+words.first.each_byte do |ascii|
+  puts ascii
+end
+=end
+
+output = File.open('output.txt', 'wb')
+
+words.each do |word|
+  output << word
+  output << [word.length].pack('C')
+end
+
+output.close
